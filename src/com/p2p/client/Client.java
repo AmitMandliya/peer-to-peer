504eb95c07453dc1e2e58900854a597cb65eb59a
Amit Mandliya <amitmanldiya.davviet@gmail.com>
2020-10-09 19:56:13 -0400

    initial structure

---
+++ src/com/p2p/client/Client.java
@@ -0,0 +1,14 @@
+package com.p2p.client;
+
+public class Client implements Runnable{
+    public Client() {
+    }
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+        
+    }
+}
---
+++ src/com/p2p/server/Server.java
@@ -0,0 +1,13 @@
+package com.p2p.server;
+
+public class Server implements Runnable{
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
